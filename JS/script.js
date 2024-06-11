bb559f32e1bd63c9785f090c22687e21d5de1107
Luis Felipe <luisfelipe-borges@hotmail.com>
2024-06-11 17:24:03 -0300

    Página de associação

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+const form = document.getElementById('form-associacao');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        alert('Você se associou com sucesso!');
+    });
