22e7a38fac6ee14cf69618cf02107d9ed1ad8a12
Luis Felipe <luisfelipe-borges@hotmail.com>
2024-06-14 14:12:47 -0300

    Atualização página associação

--- JS/script.js
+++ JS/script.js
@@ -4,3 +4,13 @@ const form = document.getElementById('form-associacao');
         event.preventDefault();
         alert('Você se associou com sucesso!');
     });
+
+    function uncheckOthers(checkbox) {
+        var checkboxes = document.getElementsByName(checkbox.name);
+        for (var i = 0; i < checkboxes.length; i++) {
+            if (checkboxes[i] !== checkbox) {
+                checkboxes[i].checked = false;
+            }
+        }
+    }
+    
